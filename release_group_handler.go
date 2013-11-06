16f63313755a4273c7e4914dfeca4a1a4fdc9379
Andrea Franz <andrea@gravityblast.com>
2013-11-06 11:04:14 +0000

    added artists on the release-group response

--- release_group_handler.go
+++ release_group_handler.go
@@ -8,17 +8,23 @@ import (
   "github.com/pilu/traffic"
 )
 
+type ReleaseGroupArtist struct {
+  Gid   string `json:"gid"`
+  Name  string `json:"name"`
+}
+
 type ReleaseGroup struct {
-  Gid               string  `json:"gid"`
-  Title             string  `json:"name"`
-  Comment           string  `json:"comment"`
-  FirstReleaseDate  string  `json:"firstReleaseDate"`
-  Type              string  `json:"type"`
+  Gid               string                 `json:"gid"`
+  Name              string                 `json:"name"`
+  Comment           string                 `json:"comment"`
+  FirstReleaseDate  string                 `json:"firstReleaseDate"`
+  Type              string                 `json:"type"`
+  Artists           []*ReleaseGroupArtist  `json:"artists"`
 }
 
 const FindReleaseGroupByGidQuery = `
   SELECT
-    RG.gid, RG.name, RG.comment, RGPT.name,
+    RG.gid, RG.name, RG.comment, RG.artist_credit, RGPT.name,
     RGM.first_release_date_year, RGM.first_release_date_month, RGM.first_release_date_day
   FROM
     release_group RG
@@ -29,6 +35,36 @@ const FindReleaseGroupByGidQuery = `
   WHERE
     RG.gid = $1 limit 1;`
 
+
+const FindArtistsByArtistCreditQuery = `
+  SELECT
+    A.gid, A.name
+  FROM
+    artist_credit_name ACN
+  JOIN artist A
+    on A.id = ACN.artist
+  WHERE
+    ACN.artist_credit = $1;`
+
+func FindArtistsByArtistCredit(artistCredit int) []*ReleaseGroupArtist {
+  artists := make([]*ReleaseGroupArtist, 0)
+
+  rows, err := DB.Query(FindArtistsByArtistCreditQuery, artistCredit)
+  if err != nil {
+    return artists
+  }
+
+  for rows.Next() {
+    artist := &ReleaseGroupArtist{}
+    err := rows.Scan(&artist.Gid, &artist.Name)
+    if err == nil {
+      artists = append(artists, artist)
+    }
+  }
+
+  return artists
+}
+
 func FindReleaseGroupByGid(gid string) (*ReleaseGroup, error) {
   releaseGroup := &ReleaseGroup{}
 
@@ -37,13 +73,14 @@ func FindReleaseGroupByGid(gid string) (*ReleaseGroup, error) {
   }
 
   var _type *sql.NullString
+  var artistCredit int
 
   var firstReleaseDateYear   *sql.NullInt64
   var firstReleaseDateMonth  *sql.NullInt64
   var firstReleaseDateDay    *sql.NullInt64
 
   row := DB.QueryRow(FindReleaseGroupByGidQuery, gid)
-  err := row.Scan(&releaseGroup.Gid, &releaseGroup.Title, &releaseGroup.Comment, &_type,
+  err := row.Scan(&releaseGroup.Gid, &releaseGroup.Name, &releaseGroup.Comment, &artistCredit, &_type,
                   &firstReleaseDateYear, &firstReleaseDateMonth, &firstReleaseDateDay)
 
   if err != nil {
@@ -67,6 +104,8 @@ func FindReleaseGroupByGid(gid string) (*ReleaseGroup, error) {
   if _type != nil {
     releaseGroup.Type = _type.String
   }
+
+  releaseGroup.Artists = FindArtistsByArtistCredit(artistCredit)
 
   return releaseGroup, nil
 }
