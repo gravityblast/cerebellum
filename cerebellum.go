1113735a5cf016a4aa9a92ab1a5b11aa802b8bc3
Andrea Franz <andrea@gravityblast.com>
2013-11-09 14:31:36 +0000

    optional trailing slashes

--- cerebellum.go
+++ cerebellum.go
@@ -41,36 +41,36 @@ func init() {
   router.Get("/", RootHandler)
   // Artist:
   //   /artists/056e4f3e-d505-4dad-8ec1-d04f521cbb56
-  router.Get("/artists/:gid", ArtistHandler)
+  router.Get("/artists/:gid/?", ArtistHandler)
 
   // Release Group:
   //   /release-groups/aa997ea0-2936-40bd-884d-3af8a0e064dc
   //   /artists/056e4f3e-d505-4dad-8ec1-d04f521cbb56/release-groups/aa997ea0-2936-40bd-884d-3af8a0e064dc
-  router.Get("(/artists/:artist_gid)?/release-groups/:gid", ReleaseGroupHandler).
+  router.Get("(/artists/:artist_gid)?/release-groups/:gid/?", ReleaseGroupHandler).
     AddBeforeFilter(CheckArtistFilter)
 
   // Release:
   //   /releases/79215cdf-4764-4dee-b0b9-fec1643df7c5
   //   /artists/056e4f3e-d505-4dad-8ec1-d04f521cbb56/releases/79215cdf-4764-4dee-b0b9-fec1643df7c5
-  router.Get("(/artists/:artist_gid)?/releases/:gid", ReleaseHandler)
+  router.Get("(/artists/:artist_gid)?/releases/:gid/?", ReleaseHandler)
 
   // Recordings:
   //   /releases/79215cdf-4764-4dee-b0b9-fec1643df7c5/recordings
   //   /artists/056e4f3e-d505-4dad-8ec1-d04f521cbb56/releases/79215cdf-4764-4dee-b0b9-fec1643df7c5/recordings
-  router.Get("(/artists/:artist_gid)?/releases/:release_gid/recordings", RecordingsHandler).
+  router.Get("(/artists/:artist_gid)?/releases/:release_gid/recordings/?", RecordingsHandler).
     AddBeforeFilter(CheckArtistFilter).
     AddBeforeFilter(CheckReleaseFilter)
 
   // Release Groups:
   //   /artists/056e4f3e-d505-4dad-8ec1-d04f521cbb56/release-groups
-  router.Get("/artists/:artist_gid/release-groups", ReleaseGroupsHandler).
+  router.Get("/artists/:artist_gid/release-groups/?", ReleaseGroupsHandler).
     AddBeforeFilter(CheckArtistFilter)
 
   // Recordings:
   //   /recordings/0c871a4a-efdf-47f8-98c2-cc277f806d2f
   //   /releases/79215cdf-4764-4dee-b0b9-fec1643df7c5/recordings/0c871a4a-efdf-47f8-98c2-cc277f806d2f
   //   /artists/056e4f3e-d505-4dad-8ec1-d04f521cbb56/releases/79215cdf-4764-4dee-b0b9-fec1643df7c5/recordings/833f00e1-781f-4edd-90e4-e52712618862
-  router.Get("((/artists/:artist_gid)?/releases/:release_gid)?/recordings/:gid", RecordingHandler).
+  router.Get("((/artists/:artist_gid)?/releases/:release_gid)?/recordings/:gid/?", RecordingHandler).
     AddBeforeFilter(CheckArtistFilter).
     AddBeforeFilter(CheckReleaseFilter)
 }
